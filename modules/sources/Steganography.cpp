4d40ae4006da4bf93b2264520a42492337c438b8
Ziad Osama <ziadosama@users.noreply.github.com>
2016-11-16 13:54:11 +0200

    added text and image
    to be added to file class

--- modules/sources/Steganography.cpp
+++ modules/sources/Steganography.cpp
@@ -5,12 +5,14 @@ using namespace std;
 void encode()
 {
 	//first parameter is cover file, second is the file to be embedded/hidden and the third is the password
-	system("steghide embed -cf default.jpg -ef input.txt -p test");
+	system("steghide embed -cf input.jpg -ef input.txt -p test");
+	system("steghide embed -cf default.jpg -ef input.jpg -p test");
 }
 void decode()
 {
 	//first parameter is image that we want to decrypt and the second is the key
 	system("steghide extract -sf default.jpg -p test");
+	system("steghide extract -sf input.jpg -p test");
 }
 int main()
 {
